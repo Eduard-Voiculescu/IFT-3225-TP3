7b1aeb96b553ad24ecaa92dd396872ad48a5ba05
Eduard <eduardvoiculescu95@gmail.com>
2019-04-04 13:55:10 -0400

    php script for the check on the login field in our form

---
+++ controllor/php/login_validate.php
@@ -0,0 +1,25 @@
+<?php
+
+    include('connect_database.php'); // connect to database
+    include('config.php');
+
+    // get login account
+    if (isset($_POST['login'])) {
+        $login_account = $_POST['login'];
+    } else {
+        $login_account = NULL;
+    }
+
+    $sql_reguest = "SELECT name FROM $db_table_users WHERE login='$login_account";
+    
+    $result = mysqli_query($conn, $sql_insert_request);
+
+    if ($result != NULL) { // login already in database 
+        echo json_encode(0);
+    } else { // login not in database
+        echo json_encode(1);
+    }
+
+    mysqli_close($conn);
+
+?>
