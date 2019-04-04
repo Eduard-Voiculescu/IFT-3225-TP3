485c98f4daffb275c2243fdeaabdd25eff71d034
Eduard <eduardvoiculescu95@gmail.com>
2019-04-04 13:52:31 -0400

    added values for config database file

---
+++ controllor/php/config.php
@@ -0,0 +1,9 @@
+<?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1)); ?>
+<?php
+// config.php
+$db_user = "root";
+$db_host = "localhost";
+$db_name = "cepsum";
+$db_table_users = "users";
+$db_table_reservations = "";
+?>
