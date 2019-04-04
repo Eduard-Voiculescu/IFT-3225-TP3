67be048fa8eaf42658da3fd61c7623ef6910823b
Eduard <eduardvoiculescu95@gmail.com>
2019-04-04 13:51:05 -0400

    added partial login verificaton with database

---
+++ controllor/js/login_verification.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('login_account').change(function () {
+        // call ajax to check login in database
+        validateLogin();
+    });
+});
+
+function validateLogin () {
+    var login = document.getElementById("login_account");
+    $.ajax({
+        type : "POST",
+        url: "../php/login_validate.php",
+        data : {
+            login : login
+        },
+        success: function (result) {
+            if (result == 0) {
+
+            } else if (result == 1){
+
+            }
+        }
+    });
+}
