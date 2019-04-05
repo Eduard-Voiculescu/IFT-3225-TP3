8dd6e7a3376920a0c5396d09a2526581e75c24c1
Eduard <eduardvoiculescu95@gmail.com>
2019-04-05 11:21:26 -0400

    ajax call for login account is done

--- controllor/js/login_verification.js
+++ controllor/js/login_verification.js
@@ -1,25 +1,26 @@
 $(document).ready(function() {
-    $('login_account').change(function () {
+    $('#login_account').change(function () {
         // call ajax to check login in database
         validateLogin();
     });
 });
 
 function validateLogin () {
-    var login = document.getElementById("login_account");
+    var login = document.getElementById("login_account").value;
     $.ajax({
-        type : "POST",
-        url: "../php/login_validate.php",
+        type : 'POST',
+        url: '../controllor/php/login_validate.php',
         data : {
             login : login
         },
-        success: function (result) {
-            console.log(login);
-            if (result == 0) {
-
-            } else if (result == 1){
-
-            }
+        success: function (response) {            
+            
+            if (response == 0) { // login account is not in the database
+                document.getElementById('loginAccountMessage').innerHTML = "";
+            } else if (response == 1){ // login account is in the database
+                document.getElementById('loginAccountMessage').innerHTML = "Login Account Already Exists";
+                document.getElementById('loginAccountMessage').style.color = "#ff0000";
+            }            
         }
     });
 }
