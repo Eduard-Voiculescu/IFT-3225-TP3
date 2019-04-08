9ffa3427937faa1f3a2d6784158c4eb96a36c947
unknown <eduardvoiculescu95@gmail.com>
2019-04-07 23:14:07 -0400

    once the user has been added to the database, go back to index.html

--- controllor/php/register.php
+++ controllor/php/register.php
@@ -37,4 +37,7 @@
     $sql_insert_request = "INSERT into $db_table_users (name, last_name, login, password, is_admin) VALUES ('$name', '$last_name', '$login_account', '$password', FALSE)";
     mysqli_query($conn, $sql_insert_request);
     // mysli_close($conn);
+
+    header("Location: ../../view/index.html");
+
 ?>
