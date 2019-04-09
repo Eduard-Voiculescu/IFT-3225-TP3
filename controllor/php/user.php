090b720c3517c018639bc705ffb95fd71c6dad14
Eduard <eduardvoiculescu95@gmail.com>
2019-04-09 13:21:32 -0400

    debut pour générer le html pour les user normaux

--- controllor/php/user.php
+++ controllor/php/user.php
@@ -9,10 +9,55 @@
     // get password
     $password = $_SESSION['password'];
 
-    echo "Welcome $login";
-    echo "<br />";
-    echo "Your password is $password";
+?>
 
+<html>
+    <head>
+        <!-- Begin css stylesheets-->
+        <link rel="stylesheet" type="text/css" href="../../view/style/user.css"/>
+        <link rel="stylesheet" href="../../view/style/bootstrap/css/bootstrap.min.css">
+        <!-- End css stylesheets-->
+
+        <!-- Being JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="../../view/style/bootstrap/js/bootstrap.min.js"></script>
+        <!-- End JavaScript -->
+
+        <title>
+            CEPSUM - <?php echo $login; ?>
+        </title>
+    </head>
+    <body>
+        <div class="background_image"></div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">My Reservations</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Reservation
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Book</a>
+                            <a class="dropdown-item" href="#">Cancel</a>
+                        </div>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0" action="logout.php">
+                    <button class="btn btn-primary my-2 my-sm-0" type="submit">Log Out</button>
+                </form>
+            </div>
+        </nav>
+    </body>
+</html>
+
+<?php
 //    session_destroy(); // don't forget to destroy the session once everything is generated
-
 ?>
