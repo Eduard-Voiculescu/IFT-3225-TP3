8539e605a68ec160680103ae5bdc7835904c9849
unknown <eduardvoiculescu95@gmail.com>
2019-04-14 01:15:49 -0400

    removed the useless try catch

--- controllor/php/config.php
+++ controllor/php/config.php
@@ -1,6 +1,5 @@
 <?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1)); ?>
 <?php
-try {
     // config.php
     $db_user = "root";
     $db_host = "localhost";
@@ -13,20 +12,5 @@ try {
 //    $db_name = "voiculee_cepsum";
 //    $db_table_users = "users";
 //    $db_table_reservations = "reservations";
-} catch (Exception $e) {
-    // config.php
-    $db_user = "root";
-    $db_host = "localhost";
-    $db_name = "cepsum";
-    $db_table_users = "users";
-    $db_table_reservations = "reservations";
-}
-//$db_user = "voiculee";
-//$db_host = "www-ens.iro.umontreal.ca";
-//$db_password = "Lexar4zq";
-//$db_password = "recette quelle a donnée";
-//$db_name = "voiculee_cepsum";
-//$db_table_users = "users";
-//$db_table_reservations = "reservations";
 
 ?>
