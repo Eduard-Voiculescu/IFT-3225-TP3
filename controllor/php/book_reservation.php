2c680a307888a12b67f688c9705802da60d00df6
unknown <eduardvoiculescu95@gmail.com>
2019-04-14 12:44:26 -0400

    added a check so that a user cannot book an already booked field at a given hour and date

--- controllor/php/book_reservation.php
+++ controllor/php/book_reservation.php
@@ -37,13 +37,15 @@
 
     // valid_insert is by default true
     $valid_insert = true;
+    $valid_insert_available = true;
 
-    // check the constraints before inserting
+    // check that the user has not already made a reservation that day
     $sql_insert_check_constraints_login_account = "SELECT * from $db_table_reservations WHERE login_account='$login'";
-    $result_insert_check_constraints = mysqli_query($conn, $sql_insert_check_constraints_login_account);
+    $result_insert_check_constraints_login = mysqli_query($conn, $sql_insert_check_constraints_login_account);
 
-    if ($result_insert_check_constraints) { // there is an entry of that login account in the reservations table
-        while ($row = mysqli_fetch_array($result_insert_check_constraints)) { // check each entry of that login
+
+    if ($result_insert_check_constraints_login) { // there is an entry of that login account in the reservations table
+        while ($row = mysqli_fetch_array($result_insert_check_constraints_login)) { // check each entry of that login
             if ($row['reservation_date'] == $reservation_date) {
                 $valid_insert = false;
                 break;
@@ -51,13 +53,30 @@
         }
     }
 
-    if ($valid_insert) {
-        $sql_insert_into_reservation = "INSERT into $db_table_reservations (login_account, field_number, reservation_date, reservation_hour) VALUES ('$login', '$field_number', '$reservation_date', '$reservation_hour')";
-        $result = mysqli_query($conn, $sql_insert_into_reservation);
-        header("Location: user.php");
-    } else {
-        echo "<script type=\"text/JavaScript\">
+    // check that the field is not already booked at the date and that hour
+    $sql_insert_check_constraints_reservation_not_already_made = "SELECT * from $db_table_reservations 
+                WHERE field_number='$field_number' AND reservation_date='$reservation_date' AND reservation_hour='$reservation_hour'";
+    $result_insert_check_constraints_reservation_not_already_made = mysqli_query($conn, $sql_insert_check_constraints_reservation_not_already_made);
+
+    if (mysqli_num_rows($result_insert_check_constraints_reservation_not_already_made) != 0) { // a reservation has already been made on that specific date and hour
+        $valid_insert_available = false;
+    }
+
+    if ($valid_insert_available) {
+        if ($valid_insert) {
+            $sql_insert_into_reservation = "INSERT into $db_table_reservations (login_account, field_number, reservation_date, reservation_hour) VALUES ('$login', '$field_number', '$reservation_date', '$reservation_hour')";
+            $result = mysqli_query($conn, $sql_insert_into_reservation);
+            header("Location: user.php");
+        } else {
+            echo "<script type=\"text/JavaScript\">
                 alert(\"You have already made a reservation for today and cannot make another one. Please cancel a reservation, should you want to make another one.\");
+                window.location.replace('user.php');
+             </script>";
+        }
+    } else {
+        $booked_message = "The field $field_number at $reservation_hour on $reservation_date has already been booked.";
+        echo "<script type=\"text/JavaScript\">
+                alert('$booked_message');
                 window.location.replace('user.php');
              </script>";
     }
