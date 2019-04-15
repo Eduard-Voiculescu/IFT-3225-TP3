d31a4244310112b546c2902fbd3bb2c5edb5c6c3
unknown <eduardvoiculescu95@gmail.com>
2019-04-15 00:30:00 -0400

    adding admin.php file for admin

--- controllor/php/admin.php
+++ controllor/php/admin.php
@@ -1,20 +1,44 @@
 <?php
 
-    // start the session
-    session_start();
+// start the session
+session_start();
 
-    // get login account information
-    $login = $_SESSION['login'];
+// get login account information
+$login = $_SESSION['login'];
 
-    // get password
-    $password = $_SESSION['password'];
+// get password
+$password = $_SESSION['password'];
 
-    echo "Welcome $login";
-    echo "<br />";
-    echo "Your password is $password";
-
-    //    session_destroy(); // don't forget to destroy the session once everything is generated
-
-
+// to send to other pages
+$_SESSION['login'] = $login; // pass login information to the next page
+$_SESSION['password'] = $password; // pass password information to the next page
 
 ?>
+
+<html>
+    <head>
+        <!-- Begin css stylesheets-->
+        <link rel="stylesheet" type="text/css" href="../../view/style/user.css"/>
+        <link rel="stylesheet" href="../../view/style/bootstrap/css/bootstrap.min.css">
+        <!-- End css stylesheets-->
+
+        <!-- Being JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="../../view/style/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../js/generate_user_html_template.js"></script>
+        <script src="../js/generate_admin_html_template.js"></script>
+        <script src="../js/generate_my_reservations_html.js"></script>
+        <script src="../js/divContentInformation.js"></script>
+        <script src="../js/generate_book_a_reservation_html.js"></script>
+        <script src="../js/generate_availability.js"></script>
+        <!-- End JavaScript -->
+
+        <title>
+            CEPSUM - <?php echo $login; ?>
+        </title>
+    </head>
+    <body>
+        <script>generate_admin_html_template();</script>
+        <div class="container" id="main_div"></div>
+    </body>
+</html>
