ee349ee25af929588febead71117432b24fa9433
unknown <eduardvoiculescu95@gmail.com>
2019-04-15 16:02:02 -0400

    push final

--- controllor/php/config.php
+++ controllor/php/config.php
@@ -1,16 +1,16 @@
 <?php if (isset($_GET['source'])) die(highlight_file(__FILE__, 1)); ?>
 <?php
     // config.php
-    $db_user = "root";
-    $db_host = "localhost";
-    $db_name = "cepsum";
-    $db_table_users = "users";
-    $db_table_reservations = "reservations";
-//    $db_user = "voiculee";
-//    $db_host = "www-ens.iro.umontreal.ca";
-//    $db_password = "leep114V";
-//    $db_name = "voiculee_cepsum";
+//    $db_user = "root";
+//    $db_host = "localhost";
+//    $db_name = "cepsum";
 //    $db_table_users = "users";
 //    $db_table_reservations = "reservations";
+    $db_user = "voiculee";
+    $db_host = "www-ens.iro.umontreal.ca";
+    $db_password = "leep114V";
+    $db_name = "voiculee_cepsum";
+    $db_table_users = "users";
+    $db_table_reservations = "reservations";
 
 ?>
--- controllor/php/connect_database.php
+++ controllor/php/connect_database.php
@@ -1,8 +1,8 @@
 <?php
     include('config.php'); //config
     // Connect to the database
-    $conn = mysqli_connect($db_host, $db_user, "", $db_name) or die(mysqli_connect_error()); // pour localhost
-//    $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die(mysqli_connect_error());
+//    $conn = mysqli_connect($db_host, $db_user, "", $db_name) or die(mysqli_connect_error()); // pour localhost
+    $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die(mysqli_connect_error());
     if (is_null($conn)) 
       die("problème de connection ". mysqli_error());
     
